c60ea5f524220a81480d3c35a4816c547e09ca5a
Sand-jrd <s.juillard@hotmail.fr>
2026-03-23 13:35:09 -1000

    Correct bug of v4

--- wfs2psf_v4_predict.c
+++ wfs2psf_v4_predict.c
@@ -14,81 +14,60 @@ void expand_v4_double(const double *T, double *Z, long n_samples, int nx) {
 }
 
 int main(int argc, char **argv) {
-    if (argc < 4) {
-        printf("Usage: %s <X_new.fits> <model_prefix> <out_Y.fits>\n", argv[0]);
-        return 1;
+    if (argc < 4) return 1;
+    const char *x_f = argv[1], *mod = argv[2], *out = argv[3];
+
+    double *X_r; long N_X, P_X; int xa_x, ya_x, nax_x;
+    read_fits(x_f, &X_r, &N_X, &P_X, &xa_x, &ya_x, &nax_x);
+
+    char path[1024]; int nx, xa_y, ya_y;
+    snprintf(path, 1024, "%s_v4_info.txt", mod);
+    FILE *fp = fopen(path, "r"); if(!fp) return 1;
+    fscanf(fp, "nx %d\nxa_y %d\nya_y %d\n", &nx, &xa_y, &ya_y); fclose(fp);
+
+    double *X_m, *X_s, *PCx = NULL, *B_re, *B_im;
+    long n_m, p_m, n_s, p_s, n_pc, p_pc, n_bre, p_bre, n_bim, p_bim;
+    int x, y, n;
+
+    snprintf(path, 1024, "%s_Xmean.fits", mod); read_fits(path, &X_m, &n_m, &p_m, &x, &y, &n);
+    snprintf(path, 1024, "%s_Xstd.fits", mod); read_fits(path, &X_s, &n_s, &p_s, &x, &y, &n);
+    snprintf(path, 1024, "%s_B_re.fits", mod); read_fits(path, &B_re, &n_bre, &p_bre, &x, &y, &n);
+    snprintf(path, 1024, "%s_B_im.fits", mod); read_fits(path, &B_im, &n_bim, &p_bim, &x, &y, &n);
+    
+    // PCx is optional (RAW mode skip)
+    snprintf(path, 1024, "%s_PCx.fits", mod);
+    if (access(path, F_OK) == 0) read_fits(path, &PCx, &n_pc, &p_pc, &x, &y, &n);
+
+    // X standardization
+    for (long i = 0; i < N_X; i++) for (long j = 0; j < P_X; j++) 
+        X_r[i * P_X + j] = (X_r[i * P_X + j] - X_m[j]) / X_s[j];
+
+    // nx_eff is number of columns in B_re
+    int nx_eff = (int)p_bre;
+    long P_Y = n_bre;
+
+    // T = PCA projection or raw
+    double *T;
+    if (PCx) {
+        T = (double *)malloc(N_X * nx * sizeof(double));
+        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, (int)N_X, nx, (int)P_X, 1.0, X_r, (int)P_X, PCx, nx, 0.0, T, nx);
+    } else {
+        T = X_r;
     }
 
-    const char *x_file = argv[1];
-    const char *model_prefix = argv[2];
-    const char *out_file = argv[3];
+    double *Z = (double *)malloc(N_X * nx_eff * sizeof(double));
+    expand_v4_double(T, Z, N_X, nx);
 
-    double *X = NULL;
-    long N, P_X;
-    int xa_x, ya_x, nax_x;
-    read_fits(x_file, &X, &N, &P_X, &xa_x, &ya_x, &nax_x);
+    // Predict: S_re = Z(NxK) @ B_re^T(KxP_Y) -> (NxP_Y)
+    double *S_re = (double *)malloc(N_X * P_Y * sizeof(double));
+    double *S_im = (double *)malloc(N_X * P_Y * sizeof(double));
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, (int)N_X, (int)P_Y, nx_eff, 1.0, Z, nx_eff, B_re, nx_eff, 0.0, S_re, (int)P_Y);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, (int)N_X, (int)P_Y, nx_eff, 1.0, Z, nx_eff, B_im, nx_eff, 0.0, S_im, (int)P_Y);
 
-    char path[1024];
-    double *X_mean, *X_std, *PCx, *B_re, *B_im;
-    long n1, p1, n_pix;
-    int xa, ya, nax, nx;
+    double *Y_new = (double *)malloc(N_X * P_Y * sizeof(double));
+    for (long i = 0; i < N_X * P_Y; i++) Y_new[i] = S_re[i] * S_re[i] + S_im[i] * S_im[i];
 
-    snprintf(path, 1024, "%s_v4_info.txt", model_prefix);
-    int patchsize, ny_per_patch, nxp, nyp;
-    FILE *fp = fopen(path, "r");
-    if(!fp) { fprintf(stderr, "Model info not found.\n"); return 1; }
-    fscanf(fp, "patchsize %d\nny_per_patch %d\nnxp %d\nnyp %d\nnx %d\n", &patchsize, &ny_per_patch, &nxp, &nyp, &nx);
-    fclose(fp);
-
-    snprintf(path, 1024, "%s_Xmean.fits", model_prefix);
-    read_fits(path, &X_mean, &n1, &p1, &xa, &ya, &nax);
-    snprintf(path, 1024, "%s_Xstd.fits", model_prefix);
-    read_fits(path, &X_std, &n1, &p1, &xa, &ya, &nax);
-    snprintf(path, 1024, "%s_PCx.fits", model_prefix);
-    read_fits(path, &PCx, &n1, &p1, &xa, &ya, &nax);
-    snprintf(path, 1024, "%s_B_re.fits", model_prefix);
-    long nx_eff_long;
-    read_fits(path, &B_re, &nx_eff_long, &n_pix, &xa, &ya, &nax);
-    snprintf(path, 1024, "%s_B_im.fits", model_prefix);
-    read_fits(path, &B_im, &nx_eff_long, &n_pix, &xa, &ya, &nax);
-
-    int nx_eff = (int)nx_eff_long;
-
-    // Preprocess X
-    for (long i = 0; i < N; i++) {
-        for (long j = 0; j < P_X; j++) {
-            X[i * P_X + j] = (X[i * P_X + j] - X_mean[j]) / X_std[j];
-        }
-    }
-
-    // T = X @ PCx
-    double *T = (double *)malloc(N * nx * sizeof(double));
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, (int)N, nx, (int)P_X, 1.0, X, (int)P_X, PCx, nx, 0.0, T, nx);
-
-    // Z = [1.0, T]
-    double *Z = (double *)malloc(N * nx_eff * sizeof(double));
-    expand_v4_double(T, Z, N, nx);
-
-    // Predict: Y = (Z @ B_re^T)^2 + (Z @ B_im^T)^2
-    // B_re is n_pix x nx_eff -> we need Z(N x nx_eff) @ B_re^T (nx_eff x n_pix)
-    double *S_re = (double *)malloc(N * n_pix * sizeof(double));
-    double *S_im = (double *)malloc(N * n_pix * sizeof(double));
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, (int)N, (int)n_pix, nx_eff, 1.0, Z, nx_eff, B_re, nx_eff, 0.0, S_re, (int)n_pix);
-    cblas_dgemm(CblasRowMajor, CblasTrans, CblasTrans, (int)N, (int)n_pix, nx_eff, 1.0, Z, nx_eff, B_im, nx_eff, 0.0, S_im, (int)n_pix); // Wait B is n_pix x nx_eff
-
-    // Wait above dgemm for S_im has CblasTrans for Z? No.
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, (int)N, (int)n_pix, nx_eff, 1.0, Z, nx_eff, B_im, nx_eff, 0.0, S_im, (int)n_pix);
-
-    double *Y_new = (double *)malloc(N * n_pix * sizeof(double));
-    for (long i = 0; i < N * n_pix; i++) {
-        Y_new[i] = S_re[i] * S_re[i] + S_im[i] * S_im[i];
-    }
-
-    // Note: n_pix here matches the total pixels in patches. 
-    // We should reshape it back to the original ya_y * xa_y if needed.
-    // For now we save as 2D (concatenated patches).
-    write_fits_2d(out_file, Y_new, n_pix, N);
-
-    printf("v4 predict done. Saved %s\n", out_file);
+    write_fits_3d(out, Y_new, xa_y, ya_y, (int)N_X);
+    printf("v4 predict done: %s (%dx%dx%ld)\n", out, xa_y, ya_y, N_X);
     return 0;
 }
